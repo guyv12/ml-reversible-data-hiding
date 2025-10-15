9800a000bdda74b779094e6d4af1528b93e56584
guyv12 <igorsitkobajorski@gmail.com>
2025-10-15 18:39:35 +0200

    chore(project structure): modified pyproj.toml
    added emails for pyproj.toml, and added a placeholder example

--- ml-rdhei/__main__.py
+++ ml-rdhei/__main__.py
@@ -1,7 +1,10 @@
-# placeholder
+from data import loader
+
 
 def main() -> None:
+    loader.get_image("datasets/samples/1.pgm")
     return
+
 
 if __name__ == "__main__":
     main()
--- ml-rdhei/data/loader.py
+++ ml-rdhei/data/loader.py
@@ -1,6 +1,14 @@
-# also a placeholder to check imports /w pyproject.toml
-
 import cv2
 
-#get image?
-#process image?
+
+def get_image(filename: str) -> None:
+    image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+    if image is None:
+        print(f"Error: Could not load image '{filename}'")
+        return
+
+    cv2.imshow("image", image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
