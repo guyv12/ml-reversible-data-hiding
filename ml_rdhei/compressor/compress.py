975da71625fde276f8c38c3778eb6d2cfdc94cc5
Jakub Wisniewski <kubawfcb@gmail.com>
2026-01-08 18:17:21 +0100

    feat(compressor): add delta encoding
    added delta encoding for reference pixels compression, according to the text each delta is offset by +255 to ensure non-negative values

--- ml_rdhei/compressor/compress.py
+++ ml_rdhei/compressor/compress.py
@@ -18,3 +18,12 @@ def compress_ad_classic(kernel_weights: torch.Tensor, ref_pixels: torch.Tensor, 
     # 4 add len(ad) at the beggining
 
     return bytes(ad)
+
+def delta_huffman_encode(ref_pixels: torch.Tensor):
+    ref_pixels = ref_pixels.to(torch.int16)
+
+    deltas = torch.diff(ref_pixels)
+    deltas += 255 # offset
+    encoded_deltas = torch.cat((ref_pixels[0].unsqueeze(0), deltas))
+
+    return encoded_deltas
