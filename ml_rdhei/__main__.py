eefcacffa705274ba0767c53f9b9c63cd8ae1f4f
Jakub Wisniewski <kubawfcb@gmail.com>
2026-05-11 16:42:45 +0200

    Revert "Fix(imports): Add comma in between imports"
    This reverts commit 22b0f2a4754e363e12d0fe27d724bf9da8a3f354.

---
+++ ml_rdhei/__main__.py
@@ -0,0 +1,79 @@
+from ml_rdhei.compressor.hiding import hider
+
+import data.loader as dloader
+import predictor.predict as ppredict
+import compressor.compress as ccompress
+import compressor.encryption as encryption
+import receiver.extraction as extraction
+from ml_rdhei.data.show import show_image
+
+
+def pgm_main():
+    ## sklearn model -- we use yield in torch there would be 1 loop
+    BOSSBase_loader, _ = dloader.get_loader("datasets/BOSSbase_512")
+
+    rates = 0
+    counter = 0
+    pixels = 512 * 512
+    bpp = 8
+    bits_per_image = pixels * bpp
+    K_e = "password"
+    K_h = "password"
+
+    for i, batch in enumerate(BOSSBase_loader):
+        for raw_ad in ppredict.pgm_raw_ad_sklearn(batch):
+            kernel_weights, ref_pixels, error_map = raw_ad
+            ad = ccompress.compress_pgm_ad((512, 512), kernel_weights, ref_pixels, error_map)
+            ad_enrypted = encryption.encrypt_ad(ad, pixels, bpp, K_e)
+
+            available_bits = bits_per_image - (len(ad) * bpp)
+            emb_rate = available_bits / pixels
+            rates += emb_rate
+            counter += 1
+            print(f"Batch:{i} | Ad Length: {len(ad)}")
+            print(f"Current embedding rate[bpp]: {emb_rate:.4f}")
+            print(f"Avg embedding rate[bpp]: {rates/counter:.4f}\n")
+
+            image = hider(ad_enrypted, available_bits//8, "bardzo tajna wiadomosc", K_h)
+            extraction.ad_extraction(image, K_e, len(ref_pixels))
+            #show_image(image)
+
+    return
+
+def dicom_main():
+    DICOM_loader, _ = dloader.get_dicom_loader("datasets/DICOM")
+
+    rates = 0
+    counter = 0
+    pixels = 512 * 512
+    bpp = 16
+    bits_per_image = pixels * bpp
+
+    for i, batch in enumerate(DICOM_loader):
+        H, W = batch.shape[-2:]
+
+        for raw_ad in ppredict.dicom_raw_ad_sklearn(batch):
+            img1_error_map, img2_kernel_weights, img2_ref_pixels, img2_error_map = raw_ad
+            
+            ad = ccompress.compress_dicom_ad((H, W), img1_error_map, img2_kernel_weights, img2_ref_pixels, img2_error_map)
+
+            pixels = H * W
+            bits_per_image = pixels * bpp
+
+            available_bits = bits_per_image - (len(ad) * 8)
+            emb_rate = available_bits / pixels
+            rates += emb_rate
+            counter += 1
+            print(f"Batch:{i} | Ad Length: {len(ad)}")
+            print(f"Current embedding rate[bpp]: {emb_rate:.4f}")
+            print(f"Avg embedding rate[bpp]: {rates/counter:.4f}\n")
+    
+    return
+
+def main() -> None:
+    pgm_main()
+    dicom_main()
+
+
+if __name__ == "__main__":
+    main()
