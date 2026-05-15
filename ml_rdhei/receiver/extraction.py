40ef6ffd5dd3f2a9c29616663d941977d3716e00
Jakub Wisniewski <kubawfcb@gmail.com>
2026-05-15 18:19:55 +0200

    feat(extraction): add huffman decoding
    implement huffman decoding for reference pixels and error map;
    all fields are fully extracted now

--- ml_rdhei/receiver/extraction.py
+++ ml_rdhei/receiver/extraction.py
@@ -1,5 +1,7 @@
 import math
 import struct
+
+import torch
 from bitarray import bitarray
 from ml_rdhei.compressor.encryption import encrypt_data
 
@@ -18,18 +20,31 @@ def ad_extraction(bitstream: bytes, key: str, n_ref: int, n: int = 512 * 512, bp
     ad = encrypt_data(ad, key)  # decrypting
 
     # Kernel weights
-    weights_float = weights_extraction(ad, k)
+    weights_float, ad = weights_extraction(ad, k)
 
     # Compressed reference pixels
     b_sym = 9
     header_length_pixels = math.ceil(math.log2(n_ref * b_sym))
-    codebook_pixels, compressed_pixels = huffman_extraction(ad, b_sym, header_length_pixels)
+    codebook_pixels, compressed_pixels, ad = huffman_extraction(ad, b_sym, header_length_pixels)
 
     # Compressed error map
     header_length_error = math.ceil(math.log2((n - n_ref) * b_sym))
-    codebook_error, compressed_error = huffman_extraction(ad, b_sym, header_length_error)
+    codebook_error, compressed_error, ad = huffman_extraction(ad, b_sym, header_length_error)
 
-    return weights_float, codebook_pixels, compressed_pixels, codebook_error, compressed_error
+    # Decode Huffman
+    ref_pixels = huffman_decode(codebook_pixels, compressed_pixels)
+    error_map = huffman_decode(codebook_error, compressed_error)
+
+    # remove offset
+    deltas = [ref_pixels[0]]
+    for p in ref_pixels[1:]:
+        deltas.append(p-255)
+    error_map = [e - 255 for e in error_map]
+
+    # remove delta encoding
+    pixels = delta_decoding(deltas)
+
+    return weights_float, pixels, error_map
 
 
 def huffman_extraction(ad: bitarray, b_sym: int, header_length: int):
@@ -39,7 +54,7 @@ def huffman_extraction(ad: bitarray, b_sym: int, header_length: int):
     codebook = ad[:header_int]
     ad = ad[header_int:]
 
-    extracted_codebook: [(int, str)] = []
+    extracted_codebook: dict = {}
 
     while len(codebook) > 0:
         value = codebook[:b_sym]
@@ -53,15 +68,15 @@ def huffman_extraction(ad: bitarray, b_sym: int, header_length: int):
         code = (codebook[:code_length_int]).to01()
         codebook = codebook[code_length_int:]
 
-        extracted_codebook.append((value_int, code))
+        extracted_codebook.update({code: value_int})
 
     header = ad[:header_length]
     header_int = int(header.to01(), 2)
     ad = ad[header_length:]
-    compressed_data = ad[:header_int]
+    compressed_data = (ad[:header_int]).to01()
     ad = ad[header_int:]
 
-    return extracted_codebook, compressed_data
+    return extracted_codebook, compressed_data, ad
 
 def weights_extraction(ad: bitarray, k: int):
     weights_float = []
@@ -72,4 +87,29 @@ def weights_extraction(ad: bitarray, k: int):
         weights_float.append(weight_float)
         ad = ad[64:]
 
-    return weights_float
+    return weights_float, ad
+
+def huffman_decode(codebook: [(str, int)], compressed_data: str):
+    decoded = []
+    buffer = ""
+
+    for bit in compressed_data:
+        buffer += bit
+
+        if buffer in codebook:
+            symbol = codebook[buffer]
+            decoded.append(symbol)
+            buffer = ""
+
+    return decoded
+
+def delta_decoding(deltas: [int]):
+    pixels = []
+    current_pixel = deltas[0]
+    pixels.append(current_pixel)
+
+    for i in range(1, len(deltas)):
+        current_pixel += deltas[i]
+        pixels.append(current_pixel)
+
+    return pixels
