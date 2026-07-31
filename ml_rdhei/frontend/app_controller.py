7c7752eefbded5e93407fdf229485b0a793eaa84
Konrad Machura <konrad.machura44@gmail.com>
2026-07-31 12:01:35 +0200

    feat: managing views size
    Main view should have fixed size bc we will display photos in it.
    Processing view is resizable with min size same to main view for now.

--- ml_rdhei/frontend/app_controller.py
+++ ml_rdhei/frontend/app_controller.py
@@ -1,6 +1,6 @@
-from PySide6.QtWidgets import QMainWindow, QStackedWidget
+from PySide6.QtWidgets import QMainWindow, QStackedWidget, QSizePolicy
 
-from frontend.config import APP_SHELL_SIZE
+from frontend.config import MAIN_VIEW_SIZE, MIN_PROCESSING_VIEW_SIZE, MAX_PROCESSING_VIEW_SIZE
 from frontend.views import MainView, ProcessingView, AboutDialog
 
 class AppController:
@@ -14,7 +14,7 @@ class AppController:
 		"Creating and configuring app container that display views"
 		self.app_shell = QMainWindow()
 		self.app_shell.setWindowTitle("RDHEI Application")
-		self.app_shell.setFixedSize(APP_SHELL_SIZE)
+		self.app_shell.setFixedSize(MAIN_VIEW_SIZE)
 
 		self.stack = QStackedWidget()
 		self.app_shell.setCentralWidget(self.stack)
@@ -26,15 +26,23 @@ class AppController:
 		self.stack.addWidget(self.main_view)
 		self.stack.addWidget(self.processing_view)
 		
-		self.main_view.processing_view_btn.clicked.connect(
-			lambda: stack.setCurrentWidget(self.processing_view)
-		)
-		
+		self.main_view.processing_view_btn.clicked.connect(self.show_processing_view)
 		self.main_view.about_dialog_btn.clicked.connect(self.about_dialog.exec)
+		self.processing_view.return_btn.clicked.connect(self.show_main_view)
 
-		self.processing_view.return_btn.clicked.connect(
-			lambda: self.stack.setCurrentWidget(self.main_view)
-		)
-		
+		self.show_main_view()
+
+	def show_main_view(self):
+		self.app_shell.showNormal()
+		self.app_shell.setFixedSize(MAIN_VIEW_SIZE)
+		self.stack.setCurrentWidget(self.main_view)
+
+	def show_processing_view(self):
+		self.app_shell.setMinimumSize(MIN_PROCESSING_VIEW_SIZE)
+		self.app_shell.setMaximumSize(MAX_PROCESSING_VIEW_SIZE)
+
+		self.stack.setCurrentWidget(self.processing_view)
+		self.app_shell.showMaximized()
+
 	def run(self):
 		self.app_shell.show()
--- ml_rdhei/frontend/config.py
+++ ml_rdhei/frontend/config.py
@@ -4,7 +4,9 @@ from typing import Final
 from PySide6.QtCore import QSize
 
 APP_VERSION: Final[str] = "1.0"
-APP_SHELL_SIZE: Final[QSize] = QSize(720, 540)
+MAIN_VIEW_SIZE: Final[QSize] = QSize(720, 540)
+MIN_PROCESSING_VIEW_SIZE: Final[QSize] = MAIN_VIEW_SIZE
+MAX_PROCESSING_VIEW_SIZE: Final[QSize] = QSize(16777215, 16777215)
 ABOUT_DIALOG_SIZE: Final[QSize] = QSize(300, 200)
 MENU_WIDTH: Final[int] = 320
 PHOTO_MARGIN: Final[tuple[int, int, int, int]] = (0, 0, 0, 0)
