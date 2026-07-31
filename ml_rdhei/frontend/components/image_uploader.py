1f15307d0988ecaecd22689703e672ceda8b337b
Konrad Machura <konrad.machura44@gmail.com>
2026-07-31 21:38:29 +0200

    feature: Adding image preview to ImageUploader

--- ml_rdhei/frontend/components/image_uploader.py
+++ ml_rdhei/frontend/components/image_uploader.py
@@ -1,22 +1,51 @@
-from PySide6.QtWidgets import QFrame, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog
+from pathlib import Path
+
+from PySide6.QtWidgets import (
+	QFrame, QWidget, QLabel, QFileDialog,
+	QVBoxLayout, QHBoxLayout, QStackedLayout
+)
 from PySide6.QtCore import Qt, Signal
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QPixmap
 
 from frontend.utils import load_stylesheet
-from frontend.config import STYLES_DIR
+from frontend.config import STYLES_DIR, PHOTO_MARGIN
 
 
 class ImageUploader(QFrame):
-	image_dropped = Signal(str)
+	image_dropped = Signal(object)
 
 	def __init__(self):
 		super().__init__()
+		self._image_path = None
 		self.setAcceptDrops(True)
-		self._setup_ui()
-		load_stylesheet(self, STYLES_DIR / "image_uploader.css")
+		
+		self.main_layout = QVBoxLayout(self)
+		self.main_layout.setContentsMargins(*PHOTO_MARGIN)
+		
+		self.stacked_layout = QStackedLayout()
+		self.main_layout.addLayout(self.stacked_layout)
 
-	def _setup_ui(self):
-		uploader_layout = QVBoxLayout(self)
+		self._setup_empty_view()
+		self._setup_preview_view()
+
+		self.stacked_layout.addWidget(self.empty_widget)
+		self.stacked_layout.addWidget(self.preview_widget)
+
+		load_stylesheet(self, STYLES_DIR / "image_uploader.css")
+		self._update_ui()
+
+	@property
+	def has_image(self) -> bool:
+		return self._image_path is not None
+
+	def resizeEvent(self, event):
+		super().resizeEvent(event)
+		if self.has_image:
+			self._update_ui()
+
+	def _setup_empty_view(self):
+		self.empty_widget = QWidget()
+		uploader_layout = QVBoxLayout(self.empty_widget)
 		uploader_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 		uploader_layout.setSpacing(8)
 
@@ -50,10 +79,86 @@ class ImageUploader(QFrame):
 		uploader_layout.addSpacing(6)
 		uploader_layout.addLayout(chips_layout)
 
+	def _setup_preview_view(self):
+		self.preview_widget = QWidget()
+		uploader_layout = QVBoxLayout(self.preview_widget)
+		uploader_layout.setContentsMargins(0, 0, 0, 0) 
+		uploader_layout.setSpacing(0)
+
+		# image_header_layout = QHBoxLayout()
+		# image_header_layout.setContentsMargins(10, 5, 10, 5)
+		# self.file_label = QLabel("")
+		# self.file_label.setWordWrap(True)
+		# self.file_label.setObjectName("fileLabel")
+		# self.file_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+		# image_header_layout.addWidget(self.file_label)
+		# uploader_layout.addLayout(image_header_layout)
+
+		self.photo_display = QLabel()
+		self.photo_display.setAlignment(Qt.AlignmentFlag.AlignCenter)
+		uploader_layout.addWidget(self.photo_display)
+		
+	def _update_ui(self):
+		if self.has_image:
+			# self.file_label.setText(Path(self._image_path).name)
+			self._display_scaled_image()
+			self.stacked_layout.setCurrentWidget(self.preview_widget)
+		else:
+			self.stacked_layout.setCurrentWidget(self.empty_widget)
+
+	def _display_scaled_image(self):
+		if not self._image_path:
+			return
+
+		pix = QPixmap(self._image_path)
+		if pix.isNull():
+			self.photo_display.setText("[ Photo unavailable ]")
+			return
+
+		# label_height = self.file_label.fontMetrics().height()
+
+		max_width = self.width() - 16
+		max_height = self.height() - 24
+
+		scaled_pix = pix.scaled(
+			max_width,
+			max_height,
+			Qt.AspectRatioMode.KeepAspectRatio,
+			Qt.TransformationMode.SmoothTransformation
+		)
+		self.photo_display.setPixmap(scaled_pix)
+
+	# def _update_ui(self):
+	# 	if self.has_image:
+	# 		self.file_label.setText(Path(self._image_path).name)
+	# 		pix = QPixmap(self._image_path)
+	# 		if not pix.isNull():
+
+	# 			max_width = self.width() - 16
+	#     		max_height = self.height() - self.file_label.height() - 24
+
+	# 			scaled_pix = pix.scaled(
+	# 				max_width, max_height,
+	# 				Qt.AspectRatioMode.KeepAspectRatio,
+	# 				Qt.TransformationMode.SmoothTransformation
+	# 			)
+	# 			self.photo_display.setPixmap(scaled_pix)
+	# 		else:
+	# 			self.photo_display.setText("[ Photo unavailable ]")
+
+	# 		self.stacked_layout.setCurrentWidget(self.preview_widget)
+	# 	else:
+	# 		self.stacked_layout.setCurrentWidget(self.empty_widget)
+
+	def _set_image(self, file_path: str | None):
+		self._image_path = file_path
+		self._update_ui()
+
 	def _set_drag_active(self, is_active: bool):
 		self.setProperty("drag_active", is_active)
 		self.style().unpolish(self)
 		self.style().polish(self)
+
 
 	def mousePressEvent(self, event: QMousePressEvent):
 		if event.button() == Qt.MouseButton.LeftButton:
@@ -64,8 +169,9 @@ class ImageUploader(QFrame):
 				self.tr("Image Files (*.pgm *.dcm)"),
 			)
 			if file_name and file_name.lower().endswith(('.pgm', '.dcm')):
+				self._set_image(file_name)
 				self.image_dropped.emit(file_name)
-				print("Image selected")
+				print(f"Image selected {self._image_path}")
 
 	def dragEnterEvent(self, event: QDragEnterEvent):
 		urls = event.mimeData().urls()
@@ -82,5 +188,6 @@ class ImageUploader(QFrame):
 		if urls:
 			file_name = urls[0].toLocalFile()
 			if file_name.lower().endswith(('.pgm', '.dcm')):
+				self._set_image(file_name)
 				self.image_dropped.emit(file_name)
-				print("Image selected")
+				print(f"Image selected {self._image_path}")
--- ml_rdhei/frontend/views/processing_view.py
+++ ml_rdhei/frontend/views/processing_view.py
@@ -1,6 +1,8 @@
 from PySide6.QtWidgets import QWidget, QFrame, QHBoxLayout, QVBoxLayout, QLabel, QPushButton
+from PySide6.QtCore import Qt
 
 from frontend.components.image_uploader import ImageUploader
+from frontend.config import IMAGE_UPLOADER_SIZE
 
 class ProcessingView(QWidget):
 	"""
@@ -13,62 +15,61 @@ class ProcessingView(QWidget):
 		super().__init__()
 
 		layout = QVBoxLayout(self)
-		self.label = QLabel("Image Processing View")
-		self.label.setFixedHeight(20)
+		self.title_label = QLabel("Image Processing View")
+		self.title_label.setFixedHeight(30)
 		self.return_btn = QPushButton("Return to main window")
-		layout.addWidget(self.label)
+		layout.addWidget(self.title_label)
 
 		sections_layout = QHBoxLayout()
 		layout.addLayout(sections_layout)
 
 		input_panel = QFrame()
+		input_panel.setObjectName("inputPanel")
 		input_panel.setStyleSheet("""
-			QFrame {
+			QFrame#inputPanel {
 				background-color: #2b2b2b;
 				border: 2px solid #4A90E2;
 				border-radius: 8px;
 			}
 		""")
 		
-		input_layout = QVBoxLayout()
-		input_panel.setLayout(input_layout)
+		input_layout = QVBoxLayout(input_panel)
+		input_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+		input_subtitle_label = QLabel("Input")
+		input_subtitle_label.setFixedHeight(20)
+		input_layout.addWidget(input_subtitle_label)
 
 		image_uploader = ImageUploader()
+		image_uploader.setFixedSize(IMAGE_UPLOADER_SIZE)
 		input_layout.addWidget(image_uploader)
-		sections_layout.addWidget(input_panel)
-
-
-
-
-
-
-
-
-
-		metrics_layout = QVBoxLayout()
+		
 		metrics_panel = QFrame()
+		metrics_panel.setObjectName("metricsPanel")
 		metrics_panel.setStyleSheet("""
-			QFrame {
+			QFrame#metricsPanel {
 				background-color: #2b2b2b;
 				border: 2px solid #E74C3C;
 				border-radius: 8px;
 			}
 		""")
+		metrics_layout = QVBoxLayout()
 		metrics_layout.addWidget(metrics_panel)
-		sections_layout.addLayout(metrics_layout)
 
-
-		output_layout = QVBoxLayout()
 		output_panel = QFrame()
+		output_panel.setObjectName("outputPanel")
 		output_panel.setStyleSheet("""
-			QFrame {
+			QFrame#outputPanel {
 				background-color: #2b2b2b;
 				border: 2px solid #2ECC71;
 				border-radius: 8px;
 			}
 		""")
+		output_layout = QVBoxLayout()
 		output_layout.addWidget(output_panel)
-		sections_layout.addLayout(output_layout)
+		
+		sections_layout.addWidget(input_panel, stretch=1)
+		sections_layout.addWidget(metrics_panel, stretch=1)
+		sections_layout.addWidget(output_panel, stretch=1)
 
 		layout.addWidget(self.return_btn)
 
