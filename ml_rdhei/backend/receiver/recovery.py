c7ab944e094b211af870c5c3e0e2a297faf0f72d
guyv12 <igorsitkobajorski@gmail.com>
2026-09-16 21:27:20 +0200

    fix: round the receiver prediction

--- ml_rdhei/backend/receiver/recovery.py
+++ ml_rdhei/backend/receiver/recovery.py
@@ -61,7 +61,7 @@ def recovery(weights: list[float], ref_pixels: list[int], error_map: list[int], 
         window_valid = (~target_mask)[r_start:r_end, c_start:c_end]
         valid_pixels = window[window_valid]
 
-        prediction = valid_pixels.mean()
+        prediction = np.round(valid_pixels.mean())
 
         value = int(prediction) + error_img[r, c]
         reconstructed_img[r, c] = np.clip(value,0,255)
